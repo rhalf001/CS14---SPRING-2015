d3dec35ba3e63f4e1544731da148b0addf3a34f6
rhalf001 <rhalf001@ucr.edu>
2015-05-04 21:53:15 +0000

    First commit

--- cs14_lab/lab4/lab4.h
+++ cs14_lab/lab4/lab4.h
@@ -21,17 +21,15 @@ class priority_queue
 	void push(Entry e) 
 	{ 
 		entries.push_back( e );
-		// compares the pairs in order to sort them in the vector
 		for (int i = entries.size()-1; i != 0; --i )
 		{
 		    if (entries[i].first + entries[i].second >= entries[i-1].first
 		    + entries[i-1].second) break;
-			swap(entries[i], entries[i-1]); // swaps the pairs to sort them
+			swap(entries[i], entries[i-1]); 
 	   }
 	}
 	void print_q() 
 	{
-		// loop to print the pairs in the vector(priority_queue)
 		for (unsigned i = 0; i < entries.size(); ++i)
 		{
 			cout << entries[i].first << ' ' << entries[i].second << endl;
@@ -48,65 +46,61 @@ void inorder_(int m, int n, int k, priority_queue& q)
 	else
 	{
 		Entry t(m, n);
-		q.push(t); // push the pair onto the queue
-		// recursively call inorder_ three times using the formulas given
+		q.push(t);
 	    inorder_(2 * m - n, m, k, q);
 		inorder_(2 * m + n, m, k, q);
 		inorder_(m + 2 * n, n, k, q);
 	}
 }
 	
-void inorder(int num) // function to print coprime pairs in inorder order
+void inorder(int num)
 {
-	priority_queue qu; // create que to pass into the inorder helper function
-	inorder_(2, 1, num, qu); // call the helper function
-	inorder_(3, 1, num, qu); // call the helper function
-	qu.print_q(); // print the queue
+	priority_queue qu; 
+	inorder_(2, 1, num, qu); 
+	inorder_(3, 1, num, qu); 
+	qu.print_q(); 
 }
 	
 
-void preorder_(int m, int n, int k) // preorder helper function
+void preorder_(int m, int n, int k) 
 {
-	if(m + n >= k) // base case
+	if(m + n >= k) 
 	{
 		return;
 	}
 	else
 	{
-		cout << m << ' ' << n << endl; // print the coprime pair
-		// recursively call preorder_ three times using the given formulas
+		cout << m << ' ' << n << endl;
 		preorder_(2 * m - n, m, k);
 		preorder_(2 * m + n, m, k);
 		preorder_(m + 2 * n, n, k);
 	}
 }
 	
-void preorder(int num) // function to print coprime pairs in preorder traversal
+void preorder(int num) 
 {
-    // call the preorder helper function(preorder_)
+    
 	preorder_(2, 1, num);
 	preorder_(3, 1, num);
 }
 	
-void postorder_(int m, int n, int k) // postorder helper function 
+void postorder_(int m, int n, int k) 
 {
-	if(m + n >= k) // base case
+	if(m + n >= k) 
 	{
 		return;
 	}
 	else
 	{
-		// recursively call postorder_ three times
 		postorder_(2 * m - n, m, k);
 		postorder_(2 * m + n, m, k);
 		postorder_(m + 2 * n, n, k);
-		cout << m << ' ' << n << endl; // print the coprime pair
+		cout << m << ' ' << n << endl;
 	}
 }
 	
-void postorder(int num) //function to print coprime pairs in postorder traversal
+void postorder(int num) 
 {
-	// call the postorder helper function(postorder_)
 	postorder_(2, 1, num);
 	postorder_(3, 1, num);
 }
